b5044c636dcc913c1d714aeedc3e47d3f8ca1696
Anatoliy <47372909+Trotil2001@users.noreply.github.com>
2022-12-12 21:42:21 +0800

    DZ Sem6

---
+++ HomeWork6/Task2.py
@@ -0,0 +1,31 @@
+'''
+Напишите программу, которая принимает на вход 
+вещественное число и показывает сумму его цифр.
+
+Пример:
+
+0,56 -> 11 
+'''
+
+
+def sum_num(n):
+    sum_dig = 0
+    for i in range(len(n)):
+        if (n[i] != ".") and (n[i] != ","):
+            sum_dig += int(n[i])
+        else:
+            continue
+    return sum_dig
+
+
+def sum_num1(n):
+    if "," in n:
+        print(f"Сумма его цифр: ",sum(map(int, n.replace(",",""))))  # map
+    else:
+        print(f"Сумма его цифр: ",sum(map(int, n.replace(".",""))))
+
+
+n = input("Ввведите число N: ")
+print(f"Сумма его цифр: ", sum_num(n))
+print()
+sum_num1(n)
